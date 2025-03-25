6c4e9d9b0f0b5fa4aedbeb886cdad8a1d676fdea
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:13:28 +0600

    Create MapVariables.go

---
+++ Variables/MapVariables.go
@@ -0,0 +1,10 @@
+// File Name: MapVariables.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    Capitals := map[string]string{"Bangladesh": "Dhaka", "India": "New Delhi"}
+    fmt.Println(Capitals) // Output: map[Bangladesh:Dhaka India:New Delhi]
+}
