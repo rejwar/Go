d5bb3f187edd401a3a833bad91cc14174f818724
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:12:25 +0600

    Create DefaultValues.go

---
+++ Variables/DefaultValues.go
@@ -0,0 +1,12 @@
+// File Name: DefaultValues.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    var Age int // Default value is 0
+    var Name string // Default value is an empty string
+    var IsStudent bool // Default value is false
+    fmt.Println(Age, Name, IsStudent) // Output: 0   false
+}
