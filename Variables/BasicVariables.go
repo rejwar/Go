720fe9c909d05d33ec2211b50c24c0913673eaa2
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:11:34 +0600

    Create BasicVariables.go

---
+++ Variables/BasicVariables.go
@@ -0,0 +1,12 @@
+// File Name: BasicVariables.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    var Name string = "Md"
+    var Age int = 25
+    var IsStudent bool = true
+    fmt.Println(Name, Age, IsStudent) // Output: Md 25 true
+}
