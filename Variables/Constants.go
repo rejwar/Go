b9624e87114facaaaaf9544555461fb1b42e512c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:12:42 +0600

    Create Constants.go

---
+++ Variables/Constants.go
@@ -0,0 +1,11 @@
+// File Name: Constants.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    const Pi float64 = 3.14159
+    const Greeting string = "Hello, Go!"
+    fmt.Println(Pi, Greeting) // Output: 3.14159 Hello, Go!
+}
