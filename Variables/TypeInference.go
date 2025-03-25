e30014e7d18325ba6b6e05586f71ecdb8dae7d08
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:11:50 +0600

    Create TypeInference.go

---
+++ Variables/TypeInference.go
@@ -0,0 +1,12 @@
+// File Name: TypeInference.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    Name := "Md"
+    Age := 25
+    IsStudent := true
+    fmt.Println(Name, Age, IsStudent) // Output: Md 25 true
+}
