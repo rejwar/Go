aadc2768daaa8d7b90df6e1327887de442a3a9d4
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:17:04 +0600

    Create TimeVariables.go

---
+++ Variables/TimeVariables.go
@@ -0,0 +1,13 @@
+// File Name: TimeVariables.go
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func Main() {
+    CurrentTime := time.Now()
+    fmt.Println(CurrentTime) // Output: Current date and time
+}
