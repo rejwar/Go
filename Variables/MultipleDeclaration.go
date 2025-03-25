132536222fefbb20154f2050155ef9e9ef70c10e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:12:08 +0600

    Create MultipleDeclaration.go

---
+++ Variables/MultipleDeclaration.go
@@ -0,0 +1,10 @@
+// File Name: MultipleDeclaration.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    var Name, Country string = "Md", "Bangladesh"
+    fmt.Println(Name, Country) // Output: Md Bangladesh
+}
