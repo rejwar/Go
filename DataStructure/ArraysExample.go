8adea295aee75dd8e33e28d5bc77c211e165d604
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-25 07:07:37 +0600

    Create ArraysExample.go

---
+++ DataStructure/ArraysExample.go
@@ -0,0 +1,10 @@
+// File Name: ArraysExample.go
+
+package main
+
+import "fmt"
+
+func Main() {
+    Numbers := [5]int{10, 20, 30, 40, 50} // Fixed-size array
+    fmt.Println(Numbers)                 // Output: [10 20 30 40 50]
+}
